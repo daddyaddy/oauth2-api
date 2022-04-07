ebd7c0f8ed145f7f50f0a236e11f771e201ac449
Adrian <44926230+daddyaddy@users.noreply.github.com>
2022-04-07 14:05:35 +0200

    TODO

--- src/api/accounts/accounts.routes.js
+++ src/api/accounts/accounts.routes.js
@@ -13,44 +13,9 @@ accountsRouter.get("/list", async (req, res, next) => {
 });
 
 accountsRouter.get("/me", checkToken, async (req, res, next) => {
-  const { account } = req;
-  if (!account) return next(Error("User is not logged in."));
-  res.status(200).json({ status: "success", payload: { account } });
+  // TODO
 });
 
 accountsRouter.post("/authorize", async (req, res, next) => {
-  const { code } = req.body;
-  if (!code) return next(Error("'code' is undefined"));
-
-  try {
-    const auth = new google.auth.OAuth2(
-      process.env.GOOGLE_CLIENT_ID,
-      process.env.GOOGLE_SECRET_ID,
-      process.env.GOOGLE_REDIRECT_URI
-    );
-    google.options({ auth });
-    const tokenResponse = await auth.getToken(code);
-    if (!tokenResponse.tokens) return next(Error("'code' is invalid"));
-    const { tokens: credentials } = tokenResponse;
-    auth.setCredentials(credentials);
-
-    const oauth2 = google.oauth2({ auth, version: "v2" });
-    const userInfoResponse = await oauth2.userinfo.get();
-    if (!userInfoResponse.data) return next(Error("'user' is not exist"));
-    const { data: user } = userInfoResponse;
-
-    const existedAccount = await AccountMongoModel.findOne({
-      "user.id": user.id,
-    });
-    if (!existedAccount) {
-      const newAccount = await new AccountMongoModel({ user, credentials });
-      await newAccount.save();
-    } else {
-      existedAccount.credentials = credentials;
-      await existedAccount.save();
-    }
-    res.status(200).json({ status: "success", payload: { credentials } });
-  } catch (error) {
-    return next(error);
-  }
+  // TODO
 });
--- src/middlewares/check-token.js
+++ src/middlewares/check-token.js
@@ -1,19 +1,5 @@
 import AccountMongoModel from "../models/account";
 
 export const checkToken = async (req, res, next) => {
-  const { query } = req;
-  const { access_token } = query;
-
-  if (!access_token) return next(Error("'access_token' is undefined"));
-
-  const account = await AccountMongoModel.findOne({
-    "credentials.access_token": access_token,
-  });
-  if (!account) return next(Error("'access_token' is invalid"));
-  const { expiry_date } = account.credentials;
-  const now = Date.now();
-  if (parseInt(expiry_date) < now)
-    return next(Error("'access_token' has expired"));
-  req.account = account;
-  next();
+  // TODO
 };
